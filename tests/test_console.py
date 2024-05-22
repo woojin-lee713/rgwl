c0908d657ef5fe02f339b407b0afde46dc88289a
Woo Jin Lee <163913803+woojin-lee713@users.noreply.github.com>
2024-05-22 12:17:48 -0400

    adding langauge support

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,18 +1,10 @@
 import click.testing
 import pytest
+import requests
 
 @pytest.fixture
 def runner():
     return click.testing.CliRunner()
-
-@pytest.fixture
-def mock_requests_get(mocker):
-    mock = mocker.patch("requests.get")
-    mock.return_value.__enter__.return_value.json.return_value = {
-        "title": "Lorem Ipsum",
-        "extract": "Lorem Ipsum dolor sit amet",
-    }
-    return mock
 
 from wikiapp import console
 
@@ -32,6 +24,11 @@ def test_main_faults_on_requests_error(runner, mock_requests_get):
     mock_requests_get.side_effect = Exception("Boom")
     result = runner.invoke(console.main)
     assert result.exit_code == 1
+
+def test_main_prints_message_on_requests_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = requests.RequestException
+    result = runner.invoke(console.main)
+    assert "Error" in result.output
 
 def test_main_succeeds(runner):
     # runner = click.testing.CliRunner()
---
+++ wikiapp/wikipedia.py
@@ -0,0 +1,17 @@
+import requests
+import click
+
+API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
+
+def random_page(language="en"):
+    """Get a random page in Wikipedia"""
+    try:
+        with requests.get(API_URL.format(language=language)) as response:
+            response.raise_for_status()
+            data = response.json()
+        return data
+    except requests.RequestException as error:
+        message = str(error)
+        raise click.ClickException(message) from error
+
+# Todo: handle clicks in console.py not here
